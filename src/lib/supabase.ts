6683e14745fa13f40fe9c72e8010d2d5024f1f87
aastik25 <iamneerajahuja@gmail.com>
2026-01-21 12:50:06 +0530

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,11 +1,12 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './database.types';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.https://fgohsfbvxunypadmustm.supabase.co;
+const supabaseAnonKey = import.meta.env.sb_publishable_cOxxiQmErP7u0yW7zoGHmA_qsqhH3W-;
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error('Missing Supabase environment variables');
 }
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
+
